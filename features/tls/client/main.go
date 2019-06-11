a282045884f5814aa622828fab8c5be101bbab8e
Madman <wangy8961@163.com>
2019-06-11 17:07:33 +0800

    9. gRPC TLS Secure

---
+++ features/tls/client/main.go
@@ -0,0 +1,43 @@
+// Package main implements a client for Echo service.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc/credentials"
+
+	pb "github.com/wangy8961/grpc-go-tutorial/features/echopb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:50051", "the address to connect to")
+	certFile := flag.String("cacert", "cacert.pem", "CA root certificate")
+	flag.Parse()
+
+	creds, err := credentials.NewClientTLSFromFile(*certFile, "")
+	if err != nil {
+		log.Fatalf("failed to load CA root certificate: %v", err)
+	}
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds)) // To call service methods, we first need to create a gRPC channel to communicate with the server. We create this by passing the server address and port number to grpc.Dial()
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewEchoClient(conn) // Once the gRPC channel is setup, we need a client stub to perform RPCs. We get this using the NewEchoClient method provided in the pb package we generated from our .proto.
+
+	// Contact the server and print out its response.
+	msg := "madmalls.com"
+	resp, err := c.UnaryEcho(context.Background(), &pb.EchoRequest{Message: msg}) // Now let’s look at how we call our service methods. Note that in gRPC-Go, RPCs operate in a blocking/synchronous mode, which means that the RPC call waits for the server to respond, and will either return a response or an error.
+	if err != nil {
+		log.Fatalf("failed to call UnaryEcho: %v", err)
+	}
+	fmt.Printf("response:\n")
+	fmt.Printf(" - %q\n", resp.GetMessage())
+}
---
+++ features/tls/server/main.go
@@ -0,0 +1,67 @@
+// Package main implements a server for Echo service.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/wangy8961/grpc-go-tutorial/features/echopb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+)
+
+// server is used to implement echopb.EchoServer.
+type server struct{}
+
+/*
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UnaryEcho not implemented")
+}
+*/
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	fmt.Printf("--- gRPC Unary RPC ---\n")
+	fmt.Printf("request received: %v\n", req)
+	return &pb.EchoResponse{Message: req.GetMessage()}, nil
+}
+
+func (s *server) ServerStreamingEcho(req *pb.EchoRequest, stream pb.Echo_ServerStreamingEchoServer) error {
+	return status.Errorf(codes.Unimplemented, "method ServerStreamingEcho not implemented")
+}
+
+func (s *server) ClientStreamingEcho(stream pb.Echo_ClientStreamingEchoServer) error {
+	return status.Errorf(codes.Unimplemented, "method ClientStreamingEcho not implemented")
+}
+
+func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
+	return status.Errorf(codes.Unimplemented, "method BidirectionalStreamingEcho not implemented")
+}
+
+func main() {
+	port := flag.Int("port", 50051, "the port to serve on")
+	certFile := flag.String("certfile", "server.crt", "Server certificate")
+	keyFile := flag.String("keyfile", "server.key", "Server private key")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port)) // Specify the port we want to use to listen for client requests
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Printf("server listening at %v\n", lis.Addr())
+
+	creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+	if err != nil {
+		log.Fatalf("failed to load certificates: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds)) // Create an instance of the gRPC server
+
+	pb.RegisterEchoServer(s, &server{})  // Register our service implementation with the gRPC server
+	if err := s.Serve(lis); err != nil { // Call Serve() on the server with our port details to do a blocking wait until the process is killed or Stop() is called.
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
